46fade3c8865cc4b67c0d26cbfc84750f3976aa1
Jainil-Patel <jainilp4321@gmail.com>
2026-04-09 22:35:32 +0530

    css file found

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // @ts-ignore
 import eslint from "vite-plugin-eslint";
 
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       host: "0.0.0.0",
     },
-    plugins: [react()/*, eslint()*/],
+    plugins: [tailwindcss(), react()/*, eslint()*/],
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
